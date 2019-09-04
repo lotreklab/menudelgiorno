dd5fb81c7aea3aa36894bf8f5f83ce5520cb0e11
Edoardo Grassi <edo.g@hotmail.it>
2019-09-04 16:37:29 +0200

    Added huey task for scraping menu and attributes in Courses

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 from polymorphic.models import PolymorphicModel
 
-FIRST_CONTENT_TYPES = [('0', 'Terra'), ('1', 'Mare')]
-SECOND_CONTENT_TYPES = [('0', 'Terra'), ('1', 'Insalatona')]
-SIDE_COOKING_TYPES = [('0', 'Vapore'), ('1', 'Forno'), ('2', 'Non definito')]
+FIRST_CONTENT_TYPES = [('T', 'Terra'), ('M', 'Mare')]
+FIRST_COOKING_TYPES = [('C', 'Caldo'), ('F', 'Freddo')]
+SECOND_CONTENT_TYPES = [('T', 'Terra'), ('I', 'Insalatona')]
+SIDE_COOKING_TYPES = [('V', 'Vapore'), ('F', 'Forno'), ('N', 'Non definito')]
+
 
 class Course(PolymorphicModel):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
+    notes = models.TextField(max_length=2048, null=True)
+
 
 class FirstCourse(Course):
     contentType = models.CharField(max_length=1, choices=FIRST_CONTENT_TYPES)
+    cookingType = models.CharField(max_length=1, choices=FIRST_COOKING_TYPES)
+
 
 class SecondCourse(Course):
     contentType = models.CharField(max_length=1, choices=SECOND_CONTENT_TYPES)
-    
+
+
 class SideCourse(Course):
     cookingType = models.CharField(max_length=1, choices=SIDE_COOKING_TYPES)
 
+
 class Menu(models.Model):
-    consumeDate = models.DateField(auto_now_add=True)
-    sendDate = models.DateField(auto_now_add=True)
+    consumeDate = models.DateField()
+    sendDate = models.DateTimeField()
     courses = models.ManyToManyField(Course, through='ContentMenu')
+
 
 class ContentMenu(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+
 from app import models
 from app import serializers
 
